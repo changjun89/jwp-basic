f8cd7a49169c3aa71b0eafbb702534736ced75b8
leechang0423 <leechang0423@naver.com>
2019-04-12 17:28:39 +0900

    jdbc 적용중

---
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -0,0 +1,32 @@
+package core.jdbc;
+
+import next.dao.UserDao;
+import next.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public abstract class JdbcTemplate {
+
+    public void insert(String sql) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = ConnectionManager.getConnection();
+            pstmt = con.prepareStatement(sql);
+            setParam(pstmt);
+            pstmt.executeUpdate();
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+
+    public abstract void setParam(PreparedStatement pstmt) throws SQLException;
+}
--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -8,20 +8,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 import core.jdbc.ConnectionManager;
+import core.jdbc.JdbcTemplate;
 import next.model.User;
 
 public class UserDao {
     public void insert(User user) throws SQLException {
+        String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+        JdbcTemplate template = new JdbcTemplate() {
+            @Override
+            public void setParam(PreparedStatement pstmt) throws SQLException {
+                pstmt.setString(1,user.getUserId());
+                pstmt.setString(2,user.getPassword());
+                pstmt.setString(3,user.getName());
+                pstmt.setString(4,user.getEmail());
+            }
+        };
+        template.insert(sql);
+    }
+
+
+
+    public void update(User user) throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
         try {
             con = ConnectionManager.getConnection();
-            String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+            String sql = "UPDATE USERS SET PASSWORD=? , NAME =?, EMAIL=? WHERE USERID=?";
             pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, user.getUserId());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getName());
-            pstmt.setString(4, user.getEmail());
+            pstmt.setString(1, user.getPassword());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setString(4, user.getUserId());
 
             pstmt.executeUpdate();
         } finally {
@@ -35,13 +52,36 @@ public class UserDao {
         }
     }
 
-    public void update(User user) throws SQLException {
-        // TODO 구현 필요함.
-    }
-
     public List<User> findAll() throws SQLException {
-        // TODO 구현 필요함.
-        return new ArrayList<User>();
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        List<User> userList = new ArrayList<>();
+        try {
+            con = ConnectionManager.getConnection();
+            String sql = "SELECT userId, password, name, email FROM USERS";
+            pstmt = con.prepareStatement(sql);
+
+            rs = pstmt.executeQuery();
+            User user = null;
+            while (rs.next()) {
+                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                        rs.getString("email"));
+                userList.add(user);
+            }
+
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+        return userList;
     }
 
     public User findByUserId(String userId) throws SQLException {
